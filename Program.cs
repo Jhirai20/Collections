749aa2cbebab1a62fa8d3669b5c7e82b7ad6b6aa
Jeffrey Hirai <jhirai20@gmail.com>
2020-08-03 18:39:09 -0500

    Finished

--- Program.cs
+++ Program.cs
@@ -35,14 +35,26 @@ namespace Collections
                 // Output the length of this list after building it
                     Console.WriteLine($"There are {flavors.Count} flavors!");
                 // Output the third flavor in the list, then remove this value
-                    Console.WriteLine(flavors[3]);
+                    Console.WriteLine(flavors[2]);
+                    flavors.Remove(flavors[2]);
                 // Output the new length of the list (It should just be one fewer!)
+                    Console.WriteLine($"There are now {flavors.Count} flavors!");
             // User Info Dictionary
                 // Create a dictionary that will store both string keys as well as string values
+                    Dictionary<string,string> profile = new Dictionary<string, string>();
                 // Add key/value pairs to this dictionary where:
-                // each key is a name from your names array
-                // each value is a randomly select a flavor from your flavors list.
+                    // each key is a name from your names array
+                    // each value is a randomly select a flavor from your flavors list.
+                    Random random=new Random();
+                    for (int i = 0; i < strArr.Length; i++)
+                    {
+                        profile.Add($"{strArr[i]}",$"{flavors[random.Next(0,4)]}");
+                    }
                 // Loop through the dictionary and print out each user's name and their associated ice cream flavor
+                    foreach (var item in profile)
+                    {
+                        Console.WriteLine(item);
+                    }
         }
     }
 }
